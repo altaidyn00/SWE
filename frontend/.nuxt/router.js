1f432acd29d40f63698b52924e1a109112b2ef1d
Aidyn Altay <altaidyn@MacBook-Pro.local>
2022-10-06 01:49:46 +0600

    in progress

---
+++ frontend/.nuxt/router.js
@@ -0,0 +1,53 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _18f4ea79 = () => interopDefault(import('../pages/services/index.vue' /* webpackChunkName: "pages/services/index" */))
+const _1c317c2c = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/services",
+    component: _18f4ea79,
+    name: "services"
+  }, {
+    path: "/",
+    component: _1c317c2c,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
---
+++ frontend/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <b-table :items="items">
+      <template v-slot:custom-foot="data">
+        <b-tr>
+          <b-td>Name</b-td>
+          <b-td>Surname</b-td>
+        </b-tr>
+      </template>
+    </b-table>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      items: [
+        { firstName: "alex", lastName: "green" },
+        {
+          firstName: "may",
+          lastName: "smith",
+        },
+        { firstName: "james", lastName: "jones" },
+      ],
+    };
+  },
+};
+</script>
