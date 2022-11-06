670d2376dd9fef82390c65dd88485d6cd91aa338
Aidyn Altay <altaidyn00>
2022-11-06 22:36:05 +0600

    asdsad

--- frontend/pages/create/doctor/index.vue
+++ frontend/pages/create/doctor/index.vue
@@ -204,6 +204,10 @@ export default {
     async create() {
       this.$v.form.$touch();
       if (this.$v.form.$invalid) return;
+      this.form.id = +this.form.id;
+      this.form.depID = +this.form.depID;
+      this.form.experience = +this.form.experience;
+      this.form.rating = +this.form.rating;
       const response = await this.createDoctor(this.form);
       console.log(response);
     },
